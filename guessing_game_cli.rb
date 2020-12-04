999cb72af687ef9051da927e76d73d998a329c7f
John Wisneski <john.j.wisneski@gmail.com>
2020-12-04 22:31:30 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -2,8 +2,8 @@ def run_guessing_game
   number_to_guess = rand(6)+1
   puts "Guess a number 1 through 6 dingus"
   guess = gets.chomp
-  if guess.to_i == number_to_guess
-    then puts "You guessed the correct number!"
+  if guess.downcase == "exit"
+    then puts "Goodbye!"
   elsif guess.to_i != number_to_guess
     then puts "Sorry! The computer guessed #{number_to_guess}"
   elsif guess.downcase == "exit"
